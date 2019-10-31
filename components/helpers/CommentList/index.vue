1d9be2947fe1979891d14b8a2b6a05dbdfdc3e82
Erik Badger <erikbadger777@gmail.com>
2019-10-30 21:49:55 -0500

    made comment list only show three comments by default and expandds to show more in a list

--- components/helpers/CommentList/index.vue
+++ components/helpers/CommentList/index.vue
@@ -1,28 +1,77 @@
 <template>
-    <v-list three-line>
-        <v-list-item v-for="c in comments" :key="c.id">
-            <v-list-item-avatar>
-                <v-img
-                    src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif"
-                    ></v-img>
-            </v-list-item-avatar>
-            <v-list-item-content>
-                <v-list-item-title>
-                    {{c.user.username}} 
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                    {{c.text}}
-                </v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-    </v-list>
+    <div>
+        <v-list three-line>
+            <v-list-item v-for="c in splitComments" :key="c.id">
+                <v-list-item-avatar>
+                    <v-img
+                        src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif"
+                        ></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        {{c.user.username}} 
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        {{c.text}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+        <v-layout justify-center align-cente>
+            <v-btn 
+                  text 
+                  color="primary"
+                  @click="showMore = true">
+                Show More
+            </v-btn>
+        </v-layout>
+        <v-dialog v-model="showMore">
+            <v-card>
+                <v-card-text>
+                    <v-list three-line>
+                        <v-list-item v-for="c in comments" :key="c.id">
+                            <v-list-item-avatar>
+                                <v-img
+                                    src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif"
+                                    ></v-img>
+                            </v-list-item-avatar>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{c.user.username}} 
+                                </v-list-item-title>
+                                <v-list-item-subtitle>
+                                    {{c.text}}
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
+    computed: {
+        splitComments() {
+            const comments:any[] = this.comments;
+            if(comments.length >= 3) {
+                return comments.slice(0, 3);
+            }
+            return comments;
+        }
+    },
     props:{
         comments: Array
-    }
+    },
+    data:() => ({
+        showMore:false
+    })
 });
 </script>
+
+<style scoped>
+
+</style>
